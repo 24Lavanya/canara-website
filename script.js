16ba1534d437824d08f779da656065bd3c0f2224
Lavanya <laxmilavanya364@gmail.com>
2023-12-02 20:34:37 +0530

    landing-page

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const showSection = (sectionId) => {
+    document.querySelectorAll('.hidden-section').forEach(section => {
+        section.style.display = "none";   
+    })
+    document.getElementById(sectionId + '-section').style.display = 'block';
+}
+
+
+const hideSection = (sectionId) => {
+    document.getElementById(sectionId + '-section').style.display = 'none';
+}
+
+// MODAL WINDOW
+
+const admissionBtn = document.querySelector('.show-modal');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close-modal');
+console.log(admissionBtn);
+
+const openModal = function(){
+    modal.classList.remove('hidden');
+}
+const closeModal = function() {
+    modal.classList.add('hidden');
+}
+
+admissionBtn.addEventListener('click', openModal);
+close.addEventListener('click', closeModal);
+//esc key
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key == 'Escape')
+    closeModal();
+});
