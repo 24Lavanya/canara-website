cfeb68de13543c16801ce3362d69e55cece26543
Lavanya <laxmilavanya364@gmail.com>
2023-12-03 18:57:32 +0530

    hamburger

--- script.js
+++ script.js
@@ -1,38 +1,81 @@
-
+const home = document.getElementById('home');
+function isMobileView() {
+    return window.innerWidth <= 900; // Adjust the breakpoint as needed
+  }
 const showSection = (sectionId) => {
-    document.querySelectorAll('.hidden-section').forEach(section => {
-        section.style.display = "none";
-    });
+        document.querySelectorAll('.hidden-section').forEach(section => {
+            section.style.display = "none";
+        });
     document.getElementById(sectionId + '-section').style.display = 'block';
 }
 
-const hideSection = (sectionId) => {
-    document.getElementById(sectionId + '-section').style.display = 'none';
-}
+
+home.addEventListener('mouseover', () => {
+    document.querySelectorAll('.hidden-section').forEach(section => {
+        if (section != home) {
+            section.style.display = "none";
+        
+    }
+    });
+});
+
+
+// const hideSection = (sectionId) => {
+//     if (sectionId != home) {
+//         document.getElementById(sectionId + '-section').style.display = 'none';
+        
+//     }
+// }
 
 const showSectionInSection = (sectionId) => {
-    document.querySelectorAll('.hidden-section-section').forEach(section => {
-        section.style.display = "none";
-    });
+        document.querySelectorAll('.hidden-section-section').forEach(section => {
+            section.style.display = "none";
+        });
     document.getElementById(sectionId + '-section-section').style.display = 'block';
 }
 
-const hideSectionInSection = (sectionId) => {
-    document.getElementById(sectionId + '-section-section').style.display = 'none';
-}
+// const hideSectionInSection = (sectionId) => {
+//     document.getElementById(sectionId + '-section-section').style.display = 'none';
+// }
 
-document.querySelector('.hidden-section').addEventListener('mouseleave', () => {
-    document.querySelectorAll('.hidden-section, .hidden-section-section').forEach(section => {
-        section.style.display = "none";
+document.querySelectorAll('.hidden-section').forEach(section => {
+    section.addEventListener('mouseleave', () => {
+        document.querySelectorAll('.hidden-section,.hidden-section-section').forEach(section => {
+            section.style.display = "none";
+        })
     });
 });
 
+document.querySelectorAll('.nav-ul li a').forEach(function (element) {
+    element.addEventListener('click', function (event) {
+      if (isMobileView()) {
+        event.preventDefault(); // Prevent following the link on mobile
+        showSection(this.id);
+      }
+    });
+});
+  
+document.getElementById('ham').addEventListener('click', function() {
+    document.querySelector('.nav-ul').style.transform = 'translateX(-0%)';
+});
 
-document.querySelector('.hidden-section').addEventListener('mouseleave', () => {
+document.getElementById('cross').addEventListener('click', function() {
+    document.querySelector('.nav-ul').style.transform = 'translateX(100%)';
     document.querySelectorAll('.hidden-section').forEach(section => {
-        section.style.display = "none";
-    });
+        section.style.display='none'
+    })
+    
 });
+
+if (isMobileView) {
+    console.log('done')
+    document.querySelectorAll('.hidden-section-section').forEach(section => {
+        section.style.transform='translate(0px)'
+    })
+    document.querySelectorAll('.hidden-section li').forEach(section => {
+        section.style.width = '30%'
+    })
+}
 
 // MODAL WINDOW
 
