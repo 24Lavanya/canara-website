3bbaf785ecf49347a64fdba83d9139b5c7c5bac9
Lavanya <laxmilavanya364@gmail.com>
2023-12-04 21:16:43 +0530

    responsive

--- script.js
+++ script.js
@@ -99,3 +99,95 @@ document.addEventListener('keydown', function (e) {
   if (e.key == 'Escape')
     closeModal();
 });
+
+// COUNTERRRRRRRRRRRRRRRR
+
+const workSection = document.querySelector(".section-work-data");
+const workObserver = new IntersectionObserver(
+(entries, observer) => {
+  const [entry] = entries;
+  // console.log(entry);
+
+  // if (entry.isIntersecting == false)
+  if (!entry.isIntersecting) return;
+
+  // animate number counter
+
+  const counterNum = document.querySelectorAll(".counter-numbers");
+
+  const speed = 200;
+
+  counterNum.forEach(curElem => {
+    const updateNumber = () => {
+      const targetNumber = parseInt(curElem.dataset.number);
+      // console.log(targetNumber);
+      const initialNum = parseInt(curElem.innerText);
+      // console.log(initialNum);
+
+      const incrementNumber = Math.trunc(targetNumber / speed);
+      // console.log(incrementNumber);
+
+      if (initialNum < targetNumber) {
+        curElem.innerText = `${initialNum + incrementNumber}+`;
+        setTimeout(updateNumber, 10);
+      }
+    };
+
+    updateNumber();
+  });
+
+  observer.unobserve(workSection);
+},
+{
+  root: null,
+  threshold: 0 });
+
+
+
+workObserver.observe(workSection);
+
+//slider
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    autoplay:{
+      delay:1500
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+  
+//courses
+const courseBox = document.querySelector(".course-box");
+const slideBox = document.querySelectorAll(".course-box-box");
+// console.log(slideBox);
+let count = 0;
+slideBox.forEach((slide, i) => {
+  let val = `(slide.style.left = ${i * 101}%)`;
+  console.log(val);
+});
+const goPrev = () => {
+  count--;
+  console.log("yes");
+  slideShow();
+};
+const goNext = () => {
+  count++;
+  console.log("yes");
+  slideShow();
+};
+const slideShow = () => {
+  console.log("yay");
+  slideBox.forEach((slide) => {
+    count = count === slideBox.length ? 0 : count < 0 ? slideBox.length - 1 : count;
+    slide.style.transform = `translateX(-${count * 101}%)`;
+  });
+};
+
